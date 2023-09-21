c95da226ff8091504d63c6c771232d1ddb87423a
Bojken07 <bojken.zaimi@stud.sti.se>
2023-09-21 18:18:58 +0200

    Uppgift_3

---
+++ src/com/bojken/demo/Main.java
@@ -0,0 +1,25 @@
+package com.bojken.demo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        while (true) {
+            scanner();
+        }
+    }
+      public static void scanner() {
+
+        // It never stops.
+
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Say something");
+            scan.nextLine();
+      }
+
+
+
+}
+
